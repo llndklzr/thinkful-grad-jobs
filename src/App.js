2a4833e11d6d065c18f0fd0bf1ff3c8eff150c83
Leland T. Klauzer <lklauzer@gmail.com>
2021-06-18 16:41:09 -0700

    removing boilerplate

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <h1 className="App-logo">so it begins</h1>
+        <h1 className="">Thinkful Graduate Jobs</h1>
       </header>
     </div>
+    <body>
+      <h2>Celebrating success, inspiring those who are up next</h2>
+    </body>
+    </>
   );
 }
 
