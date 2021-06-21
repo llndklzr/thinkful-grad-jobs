6ca6f37b343e04a4a887bbf7cd4e98b0a64f4e5c
Leland T. Klauzer <lklauzer@gmail.com>
2021-06-21 07:04:13 -0700

    back-end initial commit

---
+++ back-end/src/errors/notFound.js
@@ -0,0 +1,8 @@
+/**
+ * Express API "Not found" handler.
+ */
+function notFound(request, response, next) {
+  next({ status: 404, message: `Path not found: ${request.originalUrl}` });
+}
+
+module.exports = notFound;
