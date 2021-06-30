c6e785bcd586ebd8b347162824ac9deda4292bb3
Leland T. Klauzer <lklauzer@gmail.com>
2021-06-29 17:58:46 -0700

    initial story cards styling

--- front-end/src/components/Stories.js
+++ front-end/src/components/Stories.js
@@ -1,4 +1,4 @@
-import "../styles/styles.scss"
+import "../styles/styles.scss";
 import { listStories } from "../utils/apiFetcher";
 import { useState, useEffect } from "react";
 
@@ -15,15 +15,18 @@ function Stories() {
   }
 
   return (
-    <div>
-      <h2>Stories</h2>
-      {stories.map(story=> {
-        const {story_id, graduate, employer, job_title} = story
-        return <div key={story_id}>
-          <h4>{`${graduate}`}</h4>
-          <p>{`${employer}`}</p>
-          <p>{`${job_title}`}</p>
-        </div>
+    <div className="card-page">
+      {stories.map((story) => {
+        const { story_id, graduate, employer, job_title } = story;
+        return (
+          <div className="card" key={story_id}>
+            <div className="card-header">{`${graduate}`}</div>
+            <div className="card-body">
+              <p className="card-text">{`${employer}`}</p>
+              <p className="card-hover-text">{`${job_title}`}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
