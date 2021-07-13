055e7c0ed9f303914091a50d6ce6fa6005f43b78
Leland T. Klauzer <lklauzer@gmail.com>
2021-07-13 07:28:11 -0700

    dragNDrop demo created

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -7,8 +7,9 @@ export default function Navbar(){
     <>
       <Link className="navbar nav-btn" to="/">Home</Link>
       <Link className="navbar nav-btn" to="/map">Map</Link>
-      <Link className="navbar nav-btn" to="/stories">Stories</Link>
-      <Link className="navbar nav-btn" to="/new-story">New Story</Link>
+      <Link className="navbar nav-btn" to="/stories">View Stories</Link>
+      <Link className="navbar nav-btn" to="/new-story">Create Story</Link>
+      <Link className="navbar nav-btn" to="/drag-n-drop">Drag N Drop Demo</Link>
     </>
   )
 }
