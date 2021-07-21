147d1cbb1fcc9b556c7495b22cebdf9b41af85b0
Leland T. Klauzer <lklauzer@gmail.com>
2021-07-21 07:20:01 -0700

    changed status code, consistent variable names

--- back-end/src/businesses/businesses.controller.js
+++ back-end/src/businesses/businesses.controller.js
@@ -3,18 +3,18 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 //! <<------- CRUDL ------->>
 
-async function list(request, response) {
+async function list(req, res) {
   const data = await service.list();
-  response.json({ data });
+  res.json({ data });
 }
 
-async function getGradsByBusinessId(req, res, next){
-  const {businessId} = req.params;
+async function getGradsByBusinessId(req, res, next) {
+  const { businessId } = req.params;
   const result = await service.getGradsByBusinessId(businessId);
-  res.status(201).json({data: result});
+  res.status(200).json({ data: result });
 }
 
 module.exports = {
   list: [asyncErrorBoundary(list)],
-  listByBusinessId: [asyncErrorBoundary(getGradsByBusinessId)]
+  listByBusinessId: [asyncErrorBoundary(getGradsByBusinessId)],
 };
